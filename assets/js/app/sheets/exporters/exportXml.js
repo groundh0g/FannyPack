3bafca5e9a8194e862373005c101d9303466a4e9
Joseph Hall <joehall@codetopia.com>
2015-07-04 08:14:12 -0500

    Updates copyright year. Don't worry, it's still the MIT license. =)

--- assets/js/app/sheets/exporters/exportXml.js
+++ assets/js/app/sheets/exporters/exportXml.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014 Joseph B. Hall [@groundh0g]
+Copyright (c) 2015 Joseph B. Hall [@groundh0g]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
